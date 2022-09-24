8af3fa24fcfc6341822c2e34d1b7c107283a7cc7
harshad Sadakale <89791924+The-NoobCoder@users.noreply.github.com>
2022-09-24 13:21:52 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=10, b=5, c;
+		c=a+b;
+		System.out.println("Add of " +a+ " and " +b+ " is " +c);
+	}
+
+}
